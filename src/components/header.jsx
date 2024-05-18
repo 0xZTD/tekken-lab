d6253ce718daeb74c3ca1f63c657249c0c3507ec
zestx5 <buyankov.ilya@gmail.com>
2024-05-18 18:37:15 +0300

    refactor: hearder component

---
+++ src/components/header.jsx
@@ -0,0 +1,28 @@
+function Header() {
+  return (
+    <header>
+      <h1>TekkenLab</h1>
+      <Nav />
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <ul>
+      <li>
+        <a href="#">Practice</a>
+      </li>
+      <li>
+        <a href="#">Characters</a>
+      </li>
+      <li>
+        <a href="https://github.com/zestx5" target="_blank">
+          <img src="src/assets/github-mark.svg" alt="github icon" />
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Header;
