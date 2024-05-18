15f20471a915776421c6268afe595d35403a332d
zestx5 <buyankov.ilya@gmail.com>
2024-05-18 22:20:38 +0300

    add: router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./css/style.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  redirect,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: redirect("/tekkenLab") },
+  {
+    path: "/tekkenLab/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
